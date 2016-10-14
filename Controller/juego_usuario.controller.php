2d1e19de97c05d285c45abd1b930817cc13ce888
1014793 <acorrea829@misena.edu.co>
2016-10-14 13:10:26 -0500

    eewew

---
+++ Controller/juego_usuario.controller.php
@@ -0,0 +1,58 @@
+<?php
+require_once("../Model/conexion.php");
+require_once("../Model/juego_usuario.class.php");
+
+//$documento=$_SESSION["documento"];
+
+$documento="10";
+
+$accion=$_REQUEST["action"];
+ switch ($accion) {
+
+ 	case 'guardarjuego':
+ 		
+ 		 $jue_nom=$_POST['jue_nom'];
+ 		 $cons_cod=$_POST['cons_cod'];
+ 		 $cat_cod=$_POST["cat_cod"];
+ 		 $jue_desc=$_POST["jue_desc"];
+ 		 $jue_cant=$_POST["jue_cant"];
+ 		 $jue_trailer=$_POST["jue_trailer"];
+ 		 $jue_fech_public=$_POST["jue_fech_public"];
+ 		 $jue_imagen=$_POST["jue_imagen"];
+ 		 $jue_pal_clave=$_POST["jue_pal_clave"];
+     $jue_estado="activo";
+
+ 		 try {
+ 		 	Gestion_Videojuego::Guardar($documento,$jue_nom,$cons_cod,$cat_cod,$jue_desc,$jue_cant,$jue_trailer, $jue_fech_public,$jue_imagen,$jue_pal_clave,$jue_estado);
+ 		 	echo "<script>alert('Guardar con exito');
+      self.location.href='../Views/menu_usuario.php?#/mis juegos';
+      </script>";
+ 		 } catch (Exception $e) {
+ 		 	echo $e;
+ 		 }
+ 		break;
+
+
+    case 'modificar':
+   		 $usu_cod=$_POST["usu_cod"];
+   		 $jue_nom=$_POST["jue_nom"];
+   		 $cons_cod=$_POST["cons_cod"];
+   		 $cat_cod=$_POST["cat_cod"];
+   		 $jue_desc=$_POST["jue_desc"];
+   		 $jue_cant=$_POST["jue_cant"];
+   		 $jue_trailer=$_POST["jue_trailer"];
+   		 $jue_fech_public=$_POST["jue_fech_public"];
+   		 $jue_imagen=$_POST["jue_imagen"];
+   		 $jue_pal_clave=$_POST["jue_pal_clave"];
+       $jue_estado=$_POST["jue_estado"];
+       $jue_cod=$_POST["jue_cod"];
+
+   		 try {
+   		 	Gestion_Videojuego::modificar($usu_cod,$jue_nom,$cons_cod,$cat_cod,$jue_desc,$jue_cant,$jue_trailer, $jue_fech_public,$jue_imagen,$jue_pal_clave,$jue_estado,$jue_cod);
+   		 	echo "Se a modificado con exito con exito";
+   		 } catch (Exception $e) {
+   		 	echo "errror" .$e;
+   		 }
+   		break;
+ }
+?>
---
+++ Model/juego_usuario.class.php
@@ -0,0 +1,77 @@
+<?php
+class Gestion_Videojuego{
+	public static function Guardar($documento,$jue_nom,$cons_cod,$cat_cod,$jue_desc,$jue_cant,$jue_trailer, $jue_fech_public,$jue_imagen,$jue_pal_clave,$jue_estado){
+		$pdo = ConexionBD::AbrirBD();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$sql= "INSERT INTO tbl_videojuego (usu_cod,jue_nom,cons_cod,cat_cod,jue_desc,jue_cant,jue_trailer,jue_fech_public,jue_imagen, jue_pal_clave,jue_estado) values (?,?,?,?,?,?,?,?,?,?,?)";
+
+		$query= $pdo->prepare($sql);
+		$query->execute(array($documento,$jue_nom,$cons_cod,$cat_cod,$jue_desc,$jue_cant,$jue_trailer, $jue_fech_public,$jue_imagen,$jue_pal_clave,$jue_estado));
+
+		ConexionBD::DesconectarBD();
+
+	}
+
+	public static function mostrarjuego(){
+
+		$pdo = ConexionBD::AbrirBD();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$sql= "select * from tbl_videojuego";
+
+		$query= $pdo->prepare($sql);
+		$query->execute();
+
+		$result=$query->fetchALL(PDO::FETCH_BOTH);
+
+		ConexionBD::DesconectarBD();
+
+		return $result;
+	}
+
+	public static function consultarporCodigo($codigo){
+    $pdo = ConexionBD::AbrirBD();
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    $sql="select * from tbl_videojuego WHERE jue_cod=?";
+
+    $query= $pdo->prepare($sql);
+    $query->execute(array($codigo));
+
+    $result= $query->fetch(PDO::FETCH_BOTH);
+
+    ConexionBD::DesconectarBD();
+
+    return $result;
+  }
+
+	public static function modificar($usu_cod,$jue_nom,$cons_cod,$cat_cod,$jue_desc,$jue_cant,$jue_trailer, $jue_fech_public,$jue_imagen,$jue_pal_clave,$jue_estado,$jue_cod){
+		$pdo = ConexionBD::AbrirBD();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+		$sql="UPDATE tbl_videojuego SET usu_cod= ?, jue_nom=?, cons_cod= ?, cat_cod=?, jue_desc=?, jue_cant=?, jue_trailer=?, jue_fech_public=?, jue_imagen=?, jue_pal_clave=?, jue_estado=? WHERE jue_cod= ?";
+		$query= $pdo->prepare($sql);
+		$query->execute(array($usu_cod,$jue_nom,$cons_cod,$cat_cod,$jue_desc,$jue_cant,$jue_trailer, $jue_fech_public,$jue_imagen,$jue_pal_clave,$jue_estado,$jue_cod));
+
+		ConexionBD::DesconectarBD();
+	}
+
+// 	public static function consultarcedula(){
+//     $pdo = ConexionBD::AbrirBD();
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+//     $sql="select usu_cod,usu_nom from tbl_usuario WHERE usu_num_docum=?"
+
+//     $query= $pdo->prepare($sql);
+//     $query->execute();
+
+//     $result= $query->fetch(PDO::FETCH_BOTH);
+
+//     ConexionBD::DesconectarBD();
+
+//     return $result;
+//   }
+ }
+
+?>

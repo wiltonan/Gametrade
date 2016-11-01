1dd58976e35e5c785a24ee9353d90b5c9b59616a
wiltonan <wilton.anre13@hotmail.com>
2016-11-01 11:45:38 -0500

    ordenando index

--- Views/login.php
+++ Views/login.php
@@ -6,19 +6,22 @@
     <link rel="stylesheet" href="materialize/css/materialize.css" >
   </head>
   <body>
-    <form action="Controller/login.controller.php" method="post">
+    <div class="formu_login">
+      <section>
+        <form action="Controller/login.controller.php" method="post">
+          <div class="row">
+            <div class="input-field col s1.5">
+              <input type="text" class="white-text" name="usu" placeholder="Nick" autocomplete="off" required>
+            </div>
 
-      <div class="row">
-        <div class="input-field col s1.5">
-          <input type="text" class="white-text" name="usu" placeholder="Nick" autocomplete="off" required>
-        </div>
+            <div class="input-field col s1.5">
+              <input type="password" class="white-text" name="pas" placeholder="Contraseña" required>
+            </div>
 
-        <div class="input-field col s1.5">
-          <input type="password" class="white-text" name="pas" placeholder="Contraseña" required>
-        </div>
-
-        <button type="submit" class="btn blue thin" name="action" value="login" style="margin-top:25px; margin-left:6px; background-color: #00f;">Entrar</button>
-      </div>
-    </form>
+            <button type="submit" class="btn blue thin" name="action" value="login">Entrar</button>
+          </div>
+        </form>
+      </section>
+    </div>
   </body>
 </html>

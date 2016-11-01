e0162923ca7f69f5cd7a595e0463188c2282e177
1014793 <acorrea829@misena.edu.co>
2016-11-01 17:22:02 -0500

    aaa

--- Model/login.class.php
+++ Model/login.class.php
@@ -7,11 +7,9 @@
     public static function Login($usu){
       try {
         $sql = ConexionBD::AbrirBD();
-<<<<<<< HEAD
-        self::$query = "SELECT usu_nick, usu_pass, cod_rol, usu_nom, usu_cod,usu_num_docum, usu_estado FROM tbl_usuario WHERE usu_nick = ? AND usu_estado = 'activo'";
-=======
-        self::$query = "SELECT usu_nick, usu_pass, cod_rol, usu_nom, usu_num_docum, usu_estado FROM tbl_usuario WHERE usu_nick = ? ";
->>>>>>> origin/master
+
+        self::$query = "SELECT usu_nick, usu_pass, cod_rol, usu_nom, usu_cod,usu_num_docum, usu_estado FROM tbl_usuario WHERE usu_nick = ? ";
+
         self::$prepare = $sql->prepare(self::$query);
         self::$prepare->execute(array($usu));
         self::$resul = self::$prepare->fetch(PDO::FETCH_BOTH);

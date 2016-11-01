6f265f4ac52eefcab5559966b05f218cae39d192
1014793 <acorrea829@misena.edu.co>
2016-11-01 17:16:19 -0500

    ssss

--- Controller/login.controller.php
+++ Controller/login.controller.php
@@ -1,43 +1,6 @@
 <?php
   require_once '../Model/conexion.php';
   require_once '../Model/login.class.php';
-<<<<<<< HEAD
-
-  $usu = $_POST['usu'];
-  $pas = $_POST['pas'];
-
-  if (isset($_POST['i'])) {
-    try {
-      $resul = Validar::Login($usu);
-      if (password_verify($pas, $resul['usu_pass'])) {
-        session_start();
-        if ($resul['usu_estado']=="activo"){
-          if ($resul['cod_rol']==1) {
-            echo "<script>location.href='../Views/menu_admin.php' </script>";
-          }else {
-             $_SESSION['documento'] = $resul['usu_num_docum'];
-             $_SESSION['nombre'] = $resul['usu_nom'];
-             $_SESSION['codigo'] = $resul['usu_cod'];
-            echo "<script>location.href='../Views/menu_usuario.php' </script>";
-          }
-        }else {
-            echo "<script>alert('El usuario se encuentra inactivo o no existe');
-            self.location.href='../';
-            </script>";
-        }
-      }else {
-        echo "<script>alert('El usuario se encuentra inactivo o no existe');
-        self.location.href='../';
-        </script>";
-      }
-    } catch (Exception $e) {
-      echo "error" . $e->getMessage();
-    }
-  }
-// este hay que probarlo para ver como se usa;
-=======
-  session_start();
->>>>>>> origin/master
 
   $accion=$_REQUEST['action'];
     switch ($accion) {
--- Views/menu_usuario.php
+++ Views/menu_usuario.php
@@ -1,6 +1,11 @@
 <?php
   session_start();
-?>
+
+ 
+  if (!isset($_SESSION["nombre"]))   {
+  header("Location:../");
+  }
+ ?>
 
 <!DOCTYPE html>
 <html>

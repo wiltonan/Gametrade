4e4fd7bdbf0e0ea5e8c57fa88397b4408c62a38f
1014793 <acorrea829@misena.edu.co>
2016-11-09 15:37:45 -0500

    assa

--- Views/js/compar.js
+++ Views/js/compar.js
@@ -5,12 +5,12 @@ function comparar(){
 	codigo=$("#codigpinputPunnt").val();
 	punto=$("#txtpuntMenu").val();
 	puntoJueg=$("#inputPunnt").val();
-	alert(punto);
 	alert(puntoJueg);
-	 if (puntoJueg>punto) {
-	 	self.location.href="comprar.php?cmp="+codigo+"";
-	 }else{
+	 if (puntoJueg>=punto) {
 	 	//llevar a poner juego
 	 	self.location.href="../Controller/juego_usuario.controller.php?cmps="+codigo+"&action=punt"
+	 }else{
+	 	
+	 	self.location.href="comprar.php?cmp="+codigo+"";
 	 }	
 }
